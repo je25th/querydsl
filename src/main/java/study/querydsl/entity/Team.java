c4079fa1bbb1fed291ed90e1bc362fa86523d953
je25th <je25th@kakao.com>
2022-06-14 22:54:21 +0900

    예제 도메인 모델 작성

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,26 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team") //fk 업데이트 안함
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
